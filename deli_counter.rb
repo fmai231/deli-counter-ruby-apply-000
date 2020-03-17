f1a25f1f10a31eb2e9b760ba24dd68b95b606cfa
Franky Mai <frankymai91@gmail.com>
2020-03-17 01:41:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,16 +11,35 @@ def line(katz_deli)
       end
 end
 
-def take_a_number(katz_deli, person)
-      if 0 == katz_deli.length
-           katz_deli << person
-              katz_deli.each_with_index do |name, index|
-              puts "Welcome, #{name}. You are number #{index+1} in line."
-            end
-      elsif katz_deli.length > 0
-          katz_deli << person 
-              puts "Welcome, #{katz_deli[-1]}. You are number #{katz_deli.length} in line."
-      end
+
+def take_a_number(line)
+
+i = 1
+while i < line.length
+puts "you are number #{i} on line."
+i+=1
+end
+
+end
+  
+
+
+
+
+
+
+
+
+  
+      # if 0 == katz_deli.length
+      #     katz_deli << person
+      #         katz_deli.each_with_index do |name, index|
+      #         puts "Welcome, #{name}. You are number #{index+1} in line."
+      #       end
+      # elsif katz_deli.length > 0
+      #     katz_deli << person 
+      #         puts "Welcome, #{katz_deli[-1]}. You are number #{katz_deli.length} in line."
+      # end
 end
 
 def now_serving(katz_deli)
